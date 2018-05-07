d2b129e98fef34f58f665bc8d56b5efdac9cc88f
jennyc <jcroft1@student.unimelb.edu.au>
2018-05-07 13:43:30 +1000

    fixed broken nav - related to dropdown menu

--- views/javascript/log_in.js
+++ views/javascript/log_in.js
@@ -1,17 +1,35 @@
 /* Function to have submit button on login page  */
 
-document.getElementById("login_submit").onclick = function() {
-    alert("button has been clicked");
-    log_in()
-};
-
 function log_in(){
-     var button = document.getElementById("login_nav");
-    if (button.style.display == "none") {
+    console.log("login_submit has been pressed")
+    var button = document.getElementById("login_nav");
+    if (button.style.display === "none") {
         button.style.display = "block";
         document.getElementById("profile_nav").style.display = "none";
     } else {
         button.style.display = "none";
         document.getElementById("profile_nav").style.display = "block";
+    }
+}
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function showDropDown() {
+    document.getElementById("myDropDown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
 }

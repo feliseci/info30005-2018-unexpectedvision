798de90fb86c87ac8a1082d1d045164733643914
jennyc <jcroft1@student.unimelb.edu.au>
2018-05-20 00:30:27 +1000

    nav bar now sticky

--- views/javascript/sticky_nav.js
+++ views/javascript/sticky_nav.js
@@ -1,21 +1,22 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// Credit: https://www.w3schools.com/howto/howto_js_sticky_header.asp
+// Get the header
 
-// Get the navbar
-let navbar = document.getElementById("top_navigation");
+window.onscroll = function() {sticky_nav();};
 
-if(!document.getElementById("top_navigation")){
-    console.log("Error");
-}
 
-// Get the offset position of the navbar
-let sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function sticky_nav() {
+
+    const navigation = document.getElementById("myNav");
+
+    console.log(window.pageYOffset);
+
+    if (window.pageYOffset !== 0) {
+        navigation.classList.add("sticky");
     } else {
-        navbar.classList.remove("sticky");
+        navigation.classList.remove("sticky");
     }
 }

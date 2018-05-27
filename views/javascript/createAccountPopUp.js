0441887dec3b887e612509831bdce7fc1d7b87b0
jennyc <jcroft1@student.unimelb.edu.au>
2018-05-28 01:36:05 +1000

    Comments on Create Account Pop Up

--- views/javascript/createAccountPopUp.js
+++ views/javascript/createAccountPopUp.js
@@ -1,9 +1,13 @@
+/* Used to enable the Create Account popup */
+
+/* Shows Comment*/
 function accountCommentForm() {
     let form = document.getElementById("account-popup-bg");
     form.style.display="block";
     console.log("hello");
 }
 
+/* Show and/or Hide Comment - enables Pop Up effect*/
 window.onclick = function(event) {
     let form = document.getElementById("account-popup-bg");
     if (event.target === form) {
